ccf41808a83bd028a9a399240b6129d350ee0973
Bernd Pichon <Bernd.Pichon@bmw.de>
2018-09-13 14:49:25 +0200

    Add: Base-Funktions: flyingBusterDrop, kneeBazooka, reverseSoinKick

--- Player.lua
+++ Player.lua
@@ -15,6 +15,8 @@ end
 
 function Example:advance(me, enemy)
   -- called every frame
+  --local fktTable = self:getFunctions(true, true)
+  --fktTabel[0]()
 end
 
 function Example:fighter()
@@ -24,5 +26,29 @@ end
 function Example:name()
 	return "Conor JcGregor"
 end
+
+-- condition: "close" && "air"
+function Example:flyingBusterDrop()
+  local result = {}
+  result["Down"] = true
+  result["B"] = true
+  return result
+end
+
+function Example:kneeBazooka()
+  local result = {}
+  result["Left"] = true --or Right
+  result["B"] = true
+  return result
+end
+
+-- consition: "close"
+function Example:reverseSpinKick()
+  local result = {}
+  result["Left"] = true --or Right
+  result["C"] = true
+  return result
+end
+
 
 return Example
