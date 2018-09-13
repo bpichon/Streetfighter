f5220cbf2dda4444bbed1b3bb9310c1d318ae93a
Florian N. Navratil <Florian-Norbert.Navratil@bmw.de>
2018-09-13 13:55:26 +0200

    FN: added simple environment

--- Player.lua
+++ Player.lua
@@ -3,6 +3,37 @@ require("Player")
 local Example = {}
 setmetatable(Example, {__index = Player})
 local Example_mt = {__index = Example}
+local file
+
+---------------------------------------------------------------------------------------------------
+-- SIMPLE environment
+---------------------------------------------------------------------------------------------------
+function SimpleSendCommand(command, args)
+  text = "cmd=" .. command .. "|" .. args .. "\n"
+  --print (text)
+
+	--co = coroutine.create(function ()
+		-- appends a word test to the last line of the file
+	file:write(text)
+	--end)
+	
+	--coroutine.resume(co)
+end
+
+--
+-- send set signal command
+--
+function SimpleSetSignal(name, value)
+    text = "param=" .. name .. "|param=" .. value
+    SimpleSendCommand("SetSignalCmdRequest", text)
+end
+
+function SimpleSchedule(time)
+    text = "param=" .. time
+    SimpleSendCommand("ScheduleCmdRequest", text)
+end
+
+---------------------------------------------------------------------------------------------------
 
 function Example.new(player)
    local self = Player.new(player)
@@ -22,7 +53,7 @@ function Example:fighter()
 end
 
 function Example:name()
-	return "Conor JcGregor"
+	return "Conor McGregor"
 end
 
 return Example
